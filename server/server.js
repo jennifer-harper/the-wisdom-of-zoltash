9cd21c26daa39902a212d14593c91ef09b1b8925
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-05 20:08:18 +1300

    corrected initial commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs/promises')
 const express = require('express')
 const hbs = require('express-handlebars')
-const res = require('express/lib/response')
+// const res = require('express/lib/response')
 
 const server = express()
 
@@ -16,9 +16,8 @@ server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
-
 server.get('/', (req, res) => {
-  dataObj = {
+ let dataObj = {
     title: 'The Wisdom of Zoltash',
   }
   res.render('home', dataObj)
@@ -29,26 +28,65 @@ server.get('/q1', (req, res) => {
   let dataObj = {
     title: 'Question One',
   }
-  res.render('q1', dataObj) //<----- Change to 'q1'
+  res.render('q1', dataObj)
 })
-
-
-
 server.post('/q1', (req, res) => {
   const formStuff = req.body
-  const answerOne = formStuff.a1;
-  console.log(answerOne)
-  res.redirect('q1')
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+  .then((result) => {
+    // get the parsed json data
+    const parsedResult = JSON.parse(result)
+     // find all fortuneResult2
+    const fortunes = parsedResult.fortunes
+    // update each objects fortuneResult2 property
+    fortunes.forEach((fortune) => {
+      fortune.fortuneResult1 = formStuff.a1
+      // fortune.fortuneResult2 = Number(formStuff.a1)
+    })
+    //change the json data so its readable, and align back to object
+    const stringResult = JSON.stringify(parsedResult, null, 2)
+    //rewrite the file
+    return fs.writeFile(__dirname + '/data/data.json', stringResult)
+  })
+  .then(() => {
+    res.redirect('q2')
+  })
+  .catch((err) => {
+    res.status(500).send(err.message)
+  })
 })
 
 
-// Server.get for the Question two
-server.get('/q2', (request, response) => {
+//Q2 route
+server.get('/q2', (req, res) => {
   let dataObj = {
-    title: 'the Wisdom of Zoltash',
+    title: 'Question Two',
   }
-  response.render('q2', dataObj) //Make sure to change this to Question Two when we have page
+  res.render('q2', dataObj)
 })
+
+
+server.post('/q2', (req, res) => {
+  const formStuff = req.body
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+  .then((result) => {
+    const parsedResult = JSON.parse(result)
+    const fortunes = parsedResult.fortunes
+
+    fortunes.forEach((fortune) => {
+      fortune.fortuneResult2 = formStuff.a2
+    })
+    const stringResult = JSON.stringify(parsedResult, null, 2)
+    return fs.writeFile(__dirname + '/data/data.json', stringResult)
+  })
+  .then(() => {
+    res.redirect('fortune')
+  })
+  .catch((err) => {
+    res.status(500).send(err.message)
+  })
+})
+
 
 
 server.get('/fortune', (req, res) => {
@@ -57,14 +95,15 @@ server.get('/fortune', (req, res) => {
     let thing = JSON.parse(answer)
     let randomIndex = Math.floor(Math.random() * thing.fortunes.length)
     let fortuneAnswer = {
-      title: 'Here is your fortune',
+      title: 'Fortune Time',
       fortune: thing.fortunes[randomIndex] 
     }
     res.render('fortune', fortuneAnswer)
   })
-    .catch((err) => {
-      res.status(500).send(err.message)
-    })
+
+  .catch((err) => {
+    res.status(500).send(err.message)
+  })
 })
 
 module.exports = server
