49326cf180696e8d9bec2685c63796d702bece72
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-06 21:40:38 +1300

    first commit

--- server/server.js
+++ server/server.js
@@ -16,94 +16,140 @@ server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
+// server.get('/', (req, res) => {
+//  let dataObj = {
+//     title: 'The Wisdom of Zoltash',
+//   }
+//   res.render('home', dataObj)
+// })
+
+
+
 server.get('/', (req, res) => {
- let dataObj = {
-    title: 'The Wisdom of Zoltash',
-  }
-  res.render('home', dataObj)
-})
-
-// Q1 route
-server.get('/q1', (req, res) => {
-  let dataObj = {
-    title: 'Question One',
-  }
-  res.render('q1', dataObj)
-})
-server.post('/q1', (req, res) => {
-  const formStuff = req.body
-  fs.readFile(__dirname + '/data/data.json', 'utf-8')
-  .then((result) => {
-    // get the parsed json data
-    const parsedResult = JSON.parse(result)
-     // find all fortuneResult2
-    const fortunes = parsedResult.fortunes
-    // update each objects fortuneResult2 property
-    fortunes.forEach((fortune) => {
-      fortune.fortuneResult1 = formStuff.a1
-      // fortune.fortuneResult2 = Number(formStuff.a1)
-    })
-    //change the json data so its readable, and align back to object
-    const stringResult = JSON.stringify(parsedResult, null, 2)
-    //rewrite the file
-    return fs.writeFile(__dirname + '/data/data.json', stringResult)
-  })
-  .then(() => {
-    res.redirect('q2')
-  })
-  .catch((err) => {
-    res.status(500).send(err.message)
-  })
-})
-
-
-//Q2 route
-server.get('/q2', (req, res) => {
-  let dataObj = {
-    title: 'Question Two',
-  }
-  res.render('q2', dataObj)
-})
-
-
-server.post('/q2', (req, res) => {
-  const formStuff = req.body
-  fs.readFile(__dirname + '/data/data.json', 'utf-8')
-  .then((result) => {
-    const parsedResult = JSON.parse(result)
-    const fortunes = parsedResult.fortunes
-
-    fortunes.forEach((fortune) => {
-      fortune.fortuneResult2 = formStuff.a2
-    })
-    const stringResult = JSON.stringify(parsedResult, null, 2)
-    return fs.writeFile(__dirname + '/data/data.json', stringResult)
-  })
-  .then(() => {
-    res.redirect('fortune')
-  })
-  .catch((err) => {
-    res.status(500).send(err.message)
-  })
-})
-
-
-
-server.get('/fortune', (req, res) => {
   fs.readFile(__dirname + '/data/data.json', 'utf-8')
   .then((answer) => {
     let thing = JSON.parse(answer)
-    let randomIndex = Math.floor(Math.random() * thing.fortunes.length)
+
     let fortuneAnswer = {
-      title: 'Fortune Time',
-      fortune: thing.fortunes[randomIndex] 
+      title: 'welcome',
+      genress: thing.genres
     }
-    res.render('fortune', fortuneAnswer)
+    res.render('home', fortuneAnswer)
   })
 
   .catch((err) => {
     res.status(500).send(err.message)
   })
 })
+
+
+.get('/create/:theme', (req, res) => {
+  const theme = req.params.theme
+
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((contents) => {
+      const parseData = JSON.parse(contents)
+      console.log('This isnt the post', parseData.genres)
+      const storyData = parseData.genres.find(
+        (element) => element.theme === theme
+      )
+
+      console.log('get story data', storyData)
+      res.render('create', storyData)
+    })
+    .catch((err) => {
+      res.send(err.message)
+    })
+})
+
+
+
+
+
+
+
+
+// // Q1 route
+// server.get('/q1', (req, res) => {
+//   let dataObj = {
+//     title: 'Question One',
+//   }
+//   res.render('q1', dataObj)
+// })
+// server.post('/q1', (req, res) => {
+//   const formStuff = req.body
+//   fs.readFile(__dirname + '/data/data.json', 'utf-8')
+//   .then((result) => {
+//     // get the parsed json data
+//     const parsedResult = JSON.parse(result)
+//      // find all fortuneResult2
+//     const fortunes = parsedResult.fortunes
+//     // update each objects fortuneResult2 property
+//     fortunes.forEach((fortune) => {
+//       fortune.fortuneResult1 = formStuff.a1
+//       // fortune.fortuneResult2 = Number(formStuff.a1)
+//     })
+//     //change the json data so its readable, and align back to object
+//     const stringResult = JSON.stringify(parsedResult, null, 2)
+//     //rewrite the file
+//     return fs.writeFile(__dirname + '/data/data.json', stringResult)
+//   })
+//   .then(() => {
+//     res.redirect('q2')
+//   })
+//   .catch((err) => {
+//     res.status(500).send(err.message)
+//   })
+// })
+
+
+// //Q2 route
+// server.get('/q2', (req, res) => {
+//   let dataObj = {
+//     title: 'Question Two',
+//   }
+//   res.render('q2', dataObj)
+// })
+
+
+// server.post('/q2', (req, res) => {
+//   const formStuff = req.body
+//   fs.readFile(__dirname + '/data/data.json', 'utf-8')
+//   .then((result) => {
+//     const parsedResult = JSON.parse(result)
+//     const fortunes = parsedResult.fortunes
+
+//     fortunes.forEach((fortune) => {
+//       fortune.fortuneResult2 = formStuff.a2
+//     })
+//     const stringResult = JSON.stringify(parsedResult, null, 2)
+//     return fs.writeFile(__dirname + '/data/data.json', stringResult)
+//   })
+//   .then(() => {
+//     res.redirect('fortune')
+//   })
+//   .catch((err) => {
+//     res.status(500).send(err.message)
+//   })
+// })
+
+
+
+// server.get('/fortune', (req, res) => {
+//   fs.readFile(__dirname + '/data/data.json', 'utf-8')
+//   .then((answer) => {
+//     let thing = JSON.parse(answer)
+//     let randomIndex = Math.floor(Math.random() * thing.fortunes.length)
+//     let fortuneAnswer = {
+//       title: 'Fortune Time',
+//       fortune: thing.fortunes[randomIndex] 
+//     }
+//     res.render('fortune', fortuneAnswer)
+//   })
+
+//   .catch((err) => {
+//     res.status(500).send(err.message)
+//   })
+// })
 
 module.exports = server
